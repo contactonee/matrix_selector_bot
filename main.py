471b5a946d0a7dec852ed62fb19a764b8cb0df03
Ruslan Bazhenov <ruslan.bazhenov@nu.edu.kz>
2022-04-14 20:48:56 +0600

    Persistance

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import argparse
+import logging
 
 from telegram.ext import (CommandHandler, ConversationHandler, Filters,
-                          MessageHandler, Updater, PicklePersistence)
+                          MessageHandler, PicklePersistence, Updater)
 
 import stages
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger('MAIN')
+logger.setLevel(logging.DEBUG)
+
 
 def parse_args():
+
+    logger.debug('Parse args')
 
     parser = argparse.ArgumentParser()
 
@@ -19,42 +29,73 @@ def parse_args():
 
 def main():
 
-    main_conversation = ConversationHandler(
-        entry_points=[
-            CommandHandler('start', stages.start)],
-        states={
-            'CREATE_NAME': [
-                MessageHandler(Filters.text, stages.create_name)],
-            'CREATE_VALUES': [
-                MessageHandler(Filters.text, stages.create_values)],
-            'IDLE': [
-                CommandHandler('select', stages.prompt_select),
-                CommandHandler('delete', stages.prompt_delete),
-                CommandHandler('create', stages.prompt_create),
-                MessageHandler(Filters.text, stages.calculate)],
-            'SELECT': [
-                MessageHandler(Filters.text, stages.set_config)
-            ],
-            'DELETE': [
-                MessageHandler(Filters.text, stages.remove_config)
-            ],
-        },
-        fallbacks=[
-            CommandHandler('cancel', stages.force_idle)]
-    )
+    logger.debug('Program start')
 
     args = parse_args()
 
     persistance = PicklePersistence('data.pkl')
 
-    updater = Updater(args.token,
+    updater = Updater(token=args.token,
                       persistence=persistance,
                       use_context=True)
 
-    updater.dispatcher.add_handler(main_conversation)
+    updater.dispatcher.add_handler(
+        ConversationHandler(
+            name='create',
+            entry_points=[
+                CommandHandler('start', stages.start),
+                CommandHandler('create', stages.prompt_create)],
+            states={
+                'CREATE_NAME': [
+                    MessageHandler(Filters.text & ~Filters.command,
+                                   stages.create_name)],
+                'CREATE_VALUES': [
+                    MessageHandler(Filters.text & ~Filters.command,
+                                   stages.create_values)],
+            },
+            fallbacks=[
+                CommandHandler('cancel', stages.force_idle)],
+            persistent=True
+        ))
+
+    updater.dispatcher.add_handler(
+        ConversationHandler(
+            name='select',
+            entry_points=[
+                CommandHandler('select', stages.prompt_select)],
+            states={
+                'SELECT': [
+                    MessageHandler(Filters.text & ~Filters.command,
+                                   stages.set_config)
+                ]
+            },
+            fallbacks=[
+                CommandHandler('cancel', stages.force_idle)],
+            persistent=True
+        ))
+
+    updater.dispatcher.add_handler(
+        ConversationHandler(
+            name='delete',
+            entry_points=[
+                CommandHandler('delete', stages.prompt_delete)],
+            states={
+                'DELETE': [
+                    MessageHandler(Filters.text & ~Filters.command,
+                                   stages.remove_config)
+                ]
+            },
+            fallbacks=[
+                CommandHandler('cancel', stages.force_idle)],
+            persistent=True
+        ))
+
+    updater.dispatcher.add_handler(
+        MessageHandler(Filters.text & ~Filters.command,
+                       stages.calculate))
 
     updater.start_polling()
-    print('Polling')
+    logger.debug('Polling')
 
 
 if __name__ == '__main__':
--- stages.py
+++ stages.py
@@ -1,73 +1,104 @@
-from ctypes import resize
-from telegram import (InlineKeyboardButton, InlineKeyboardMarkup, Message, ReplyKeyboardMarkup, ReplyKeyboardRemove, ReplyMarkup,
-                      Update, CallbackQuery)
-from telegram.ext import CallbackContext
+import logging
+from typing import DefaultDict
+
+from telegram import Message, ReplyKeyboardMarkup, ReplyKeyboardRemove, Update
+from telegram.ext import CallbackContext, ConversationHandler
 
 import utils
 
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
+
+def stage(func):
+    def wrapper(update: Update, context: CallbackContext):
+        logger.info('@' + update.effective_user.username
+                    + (f' says: \"{update.message.text}\"'
+                       if update.message is not None
+                       else ''))
+        logger.debug(context.chat_data)
+        print(func)
+        r = func(update, context)
+        logger.debug(context.chat_data)
+        return r
+    return wrapper
+
+
+@stage
 def start(update: Update, context: CallbackContext):
 
-    msg: Message = update.message
-    update.message.reply_text(
-        "Привет! Я могу считать наборы матриц для листогиба. Давай создадим наш первый конфиг файл. Введите имя конфиг файла")
-    context.chat_data['configs'] = {}
+    logger.debug(context.chat_data)
 
-    return prompt_create(update, context)
+    # New user
+    if 'configs' not in context.chat_data:
+
+        context.chat_data['configs'] = {}
+
+        update.message.reply_text(
+            "Привет! Я могу считать наборы матриц для листогиба. Для начала создайте конфиг. Введите имя нового конфига")
+
+        return 'CREATE_NAME'
+
+    else:
+        logger.info('%s already started the bot, skip intro',
+                    update.effective_user.username)
+
+        update.message.reply_text(
+            "Привет! Я могу считать наборы матриц для листогиба.")
+        return ConversationHandler.END
 
 
+@stage
 def prompt_create(update: Update, context: CallbackContext):
 
-    msg: Message = update.message
-    msg.reply_text("Введите имя конфига")
+    update.message.reply_text("Введите имя нового конфига")
 
     return 'CREATE_NAME'
 
 
+@stage
 def create_name(update: Update, context: CallbackContext):
 
-    msg: Message = update.message
+    context.chat_data['create_config_name'] = update.message.text
 
-    context.chat_data['create_config_name'] = msg.text
-
-    msg.reply_text(
+    update.message.reply_text(
         "Отличное название! Теперь введите список доступных матриц через запятую")
 
     return 'CREATE_VALUES'
 
 
+@stage
 def create_values(update: Update, context: CallbackContext):
 
-    msg: Message = update.message
-
-    values = utils.parse_matrices_values(msg.text)
-    name = context.chat_data['create_config_name']
+    values = utils.parse_matrices_values(update.message.text)
+    name = context.chat_data.pop('create_config_name')
 
     context.chat_data['configs'][name] = values
     context.chat_data['active_config'] = name
 
+    logger.debug(context.chat_data)
+
     update.message.reply_text(
         "Конфиг сохранен! Теперь будет использоваться данный конфиг. Можете вводить желаемый гиб")
 
-    return 'IDLE'
+    return ConversationHandler.END
 
 
+@stage
 def calculate(update: Update, context: CallbackContext):
-    msg: Message = update.message
 
     values = context.chat_data['configs'][context.chat_data['active_config']]
-    target = int(msg.text)
+    target = int(update.message.text)
 
     selected, result = utils.knapsack(sorted(values, reverse=True), target)
 
-    msg.reply_text(f'Доступные матрицы: {values}\n'
-                   f'Выбор: {selected}\n'
-                   f'Результат: {result}')
+    update.message.reply_text(f'Доступные матрицы: {values}\n'
+                              f'Выбор: {selected}\n'
+                              f'Результат: {result}')
 
 
+@stage
 def prompt_select(update: Update, context: CallbackContext):
-
-    msg: Message = update.message
 
     names = list(context.chat_data['configs'].keys())
 
@@ -75,25 +106,24 @@ def prompt_select(update: Update, context: CallbackContext):
         names,
         resize_keyboard=True)
 
-    msg.reply_text("Выберит конфиг", reply_markup=keyboard)
+    update.message.reply_text("Выберит конфиг", reply_markup=keyboard)
 
     return 'SELECT'
 
 
+@stage
 def set_config(update: Update, context: CallbackContext):
 
-    msg: Message = update.message
+    context.chat_data['active_config'] = update.message.text
 
-    context.chat_data['active_config'] = msg.text
+    update.message.reply_text(f'Успешно выбран конфиг {update.message.text}',
+                              reply_markup=ReplyKeyboardRemove())
 
-    msg.reply_text(f'Успешно выбран конфиг {msg.text}')
-
-    return 'IDLE'
+    return ConversationHandler.END
 
 
+@stage
 def prompt_delete(update: Update, context: CallbackContext):
-
-    msg: Message = update.message
 
     names = list(context.chat_data['configs'].keys())
 
@@ -101,26 +131,30 @@ def prompt_delete(update: Update, context: CallbackContext):
         names,
         resize_keyboard=True)
 
-    msg.reply_text("Выберит конфиг который хотите удалить",
-                   reply_markup=keyboard)
+    update.message.reply_text("Выберит конфиг который хотите удалить",
+                              reply_markup=keyboard)
 
     return 'DELETE'
 
 
+@stage
 def remove_config(update: Update, context: CallbackContext):
 
-    msg: Message = update.message
+    context.chat_data['configs'].pop(update.message.text)
 
-    context.chat_data['configs'].pop(msg.text)
-
-    if context.chat_data['active_config'] == msg.text:
+    if context.chat_data['active_config'] == update.message.text:
         context.chat_data['active_config'] = list(
             context.chat_data['configs'].keys())[0]
 
-    msg.reply_text(f'Конфиг {msg.text} удален')
+    update.message.reply_text(f'Конфиг {update.message.text} удален',
+                              reply_markup=ReplyKeyboardRemove())
 
-    return 'IDLE'
+    return ConversationHandler.END
 
 
+@stage
 def force_idle(update: Update, context: CallbackContext):
-    return 'IDLE'
+
+    update.message.reply_text('Отмена', reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
